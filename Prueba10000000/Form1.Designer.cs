4158f9a18c05a25d83fe2a70c92289b72dfabb73
RodrigoLinkIP <rodrigolopezlopez2006@gmail.com>
2022-05-04 18:08:43 -0600

    ask vñ flafls lk

--- Prueba10000000/Form1.Designer.cs
+++ Prueba10000000/Form1.Designer.cs
@@ -35,19 +35,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(368, 22);
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(337, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(99, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hola";
+            this.label1.Text = "Ejercicio 2";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(161, 151);
+            this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(342, 174);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(108, 35);
             this.button1.TabIndex = 1;
-            this.button1.Text = "AAAAAAAA";
+            this.button1.Text = "Comprobar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
